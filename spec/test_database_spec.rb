c382336426d111055bef3f8fe5e9f3abd7cc51cd
Lucas Aguilar <lucasaguilar835@gmail.com>
2024-03-07 12:53:58 -0300

    Remove comentários do código

--- spec/test_database_spec.rb
+++ spec/test_database_spec.rb
@@ -6,26 +6,22 @@ RSpec.describe 'Database' do
 
   describe 'Table Creation' do
     before(:all) do
-      # Conectar ao banco de dados de teste
       @conn = PG.connect(
         dbname: 'test_db',
         user: 'postgres',
         password: 'postgres',
-        host: 'test_postgres' # Nome do serviço do banco de dados de teste definido no docker-compose.yml
+        host: 'test_postgres'
       )
 
-      # Criar a tabela para teste
       @conn.exec("CREATE TABLE test_table (id SERIAL PRIMARY KEY, name VARCHAR(255), age INT)")
     end
 
     after(:all) do
-      # Remover a tabela após o teste
       @conn.exec("DROP TABLE IF EXISTS test_table")
       @conn.close if @conn
     end
 
     it 'creates a table successfully' do
-      # Verificar se a tabela foi criada com sucesso
       expect(table_exists?('test_table')).to be(true)
     end
   end
