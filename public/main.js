564a43ae546a98c6ac664f4e7d82ecffd01e1a5f
Lucas Aguilar <lucasaguilar835@gmail.com>
2024-03-11 16:26:19 -0300

    Adiciona visualização AJAX dos exames brutos na root

---
+++ public/main.js
@@ -0,0 +1,18 @@
+const fragment = new DocumentFragment();
+const url = 'http://localhost:3000/tests';
+
+fetch(url).
+  then((response) => response.json()).
+  then((data) => {
+    data.forEach(function(record) {
+      const li = document.createElement('li');
+      li.textContent = `${record.nome_paciente}`;
+      fragment.appendChild(li);
+    })
+  }).
+  then(() => {
+    document.querySelector('ul').appendChild(fragment);
+  }).
+  catch((error) => {
+    console.log(error);
+  });
--- server.rb
+++ server.rb
@@ -3,6 +3,12 @@ require_relative 'app/models/medical_record.rb'
 
 get '/tests' do
   content_type :json
+  response.headers['Access-Control-Allow-Origin'] = '*'
   records = MedicalRecord.all
   records.to_json
+end
+
+get '/' do
+  content_type :html
+  File.open('app/views/index.html')
 end
