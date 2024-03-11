28f6203a5b6b5494290e4e1efd9da71b21f1990b
Lucas Aguilar <lucasaguilar835@gmail.com>
2024-03-11 09:56:50 -0300

    Move configuração do Puma para o docker compose

--- server.rb
+++ server.rb
@@ -1,17 +1,8 @@
 require 'sinatra'
-require 'rack/handler/puma'
 require_relative 'app/models/medical_record.rb'
 
 get '/tests' do
   content_type :json
   records = MedicalRecord.all
   records.to_json
-end
-
-if ENV['RACK_ENV'] != 'test'
-  Rack::Handler::Puma.run(
-    Sinatra::Application,
-    Port: 3000,
-    Host: '0.0.0.0'
-  )
 end
