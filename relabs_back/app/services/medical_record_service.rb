ff001a36450022856713eb56877246fe46aab80a
Lucas Aguilar <lucasaguilar835@gmail.com>
2024-03-12 00:29:16 -0300

    Refatora service para reaproveitar métodos da model MedicalRecord

--- relabs_back/app/services/medical_record_service.rb
+++ relabs_back/app/services/medical_record_service.rb
@@ -4,16 +4,13 @@ module MedicalRecordService
     require './app/models/medical_record'
     require_relative '../../db/database_connection'
 
-    DIACRITICS = [*0x1DC0..0x1DFF, *0x0300..0x036F, *0xFE20..0xFE2F].pack('U*')
-
     def self.import(csv_path, reset_table = false)
       @db_conn = DatabaseConnection.new
 
       @csv_path = csv_path
       MedicalRecordService::DropTable.drop(@db_conn) if reset_table && table_exists?
       MedicalRecordService::CreateTable.create(@db_conn) unless table_exists?
-      prepare_dataset
-      insert_into_database
+      insert_records_into_database
 
       @db_conn.close
     end
@@ -32,38 +29,19 @@ module MedicalRecordService
         SQL
 
         query_return = @db_conn.exec(query)
-
         query_return[0]['exists'] == 't'
       end
 
-      def set_table_attributes
-        csv_attributes = CSV.read(@csv_path, col_sep: ';')[0]
-        MedicalRecord.attributes = csv_attributes.map { |attr| serialize_attribute(attr) }
-      end
-      
-      def serialize_attribute(str)
-        str
-          .unicode_normalize(:nfd)
-          .tr(DIACRITICS, '')
-          .unicode_normalize(:nfc)
-          .gsub(/[^a-zA-Z0-9]/, '_')
-      end
-
-      def prepare_dataset
+      def insert_records_into_database
         csv_patients = CSV.read(@csv_path, col_sep: ';')[1..]
-      
-        @converted_values = csv_patients.map do |patient|
-          patient.map { |attr| "\'#{serialize_values(attr)}\'" }.join(',')
-        end.join('), (')
-      end
-      
-      def serialize_values(str)
-        str
-          .gsub(/'/, "''")
-      end
 
-      def insert_into_database
-        @db_conn.exec("INSERT INTO medical_record (#{MedicalRecord.attributes.join(', ')}) VALUES (#{@converted_values})")
+        medical_records = csv_patients.map do |patient|
+          patient = patient.map { |attr| "#{@db_conn.escape(attr)}" }
+          data = Hash[MedicalRecord.attributes.zip(patient)]
+          MedicalRecord.new(data)
+        end
+
+        MedicalRecord.insert_into_database(@db_conn, medical_records)
       end
     end
   end
